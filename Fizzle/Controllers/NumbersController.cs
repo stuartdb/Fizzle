49659904bb7e888c897a8ff4723950231fb09a78
Stuart Baker <stuart@baker.nz>
2020-09-16 00:01:42 +1200

    Add support for auto fizzle

--- Fizzle/Controllers/NumbersController.cs
+++ Fizzle/Controllers/NumbersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Fizzle.Data;
+using Fizzle.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -41,18 +42,19 @@ namespace Fizzle.Controllers
         // GET: NumbersController/Transform/10
         /// <summary>
         /// Details isn't a very interesting action since it uses the id.        
-        /// Being able to dump a number in the url and see if it's been Fizzle'd is more interesting
+        /// Being able to dump a number in the url and see it be Fizzled is more fun
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public async Task<IActionResult> Transform(int id)
+        public IActionResult Transform(int id)
         {
-            var number = await _context.Numbers
-                .FirstOrDefaultAsync(n => n.Initial == id);
+            var number = new Number { Initial = id };
+            number.Fizzle();
 
             ViewData["Title"] = "Transform";
+            
             // Reuse the details view
-            return View("Details", number);
+            return View(number);
         }
     }
 }
